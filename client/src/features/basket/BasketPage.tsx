e8672ec8b0d285b4bddb5db0fc5862067d846f6f
Ankit Bharat Gujar <91015223+AKG3000@users.noreply.github.com>
2022-10-03 09:06:36 +0530

    Addition of Basket Page

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,33 +1,38 @@
 import { Add, Delete, Remove } from "@mui/icons-material";
 import { LoadingButton } from "@mui/lab";
-import { Box, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Box, Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useState } from "react";
 import agent from "../../app/api/agent";
 import { useStoreContext } from "../../app/context/StoreContext";
+import BasketSummary from "./BasketSummary";
 
 export default function BasketPage(){
     const {basket,setBasket,removeItem} = useStoreContext();
-    const [loading,setLoading] = useState(false);
-    function handleAddItem(productId :number){
-      setLoading(true);
+    const [status,setStatus] = useState({
+      loading : false,
+      name :""
+    })
+    function handleAddItem(productId :number,name:string){
+      setStatus({loading:true,name});
       agent.Basket.addItem(productId)
       .then(basket=>setBasket(basket))
       .catch(error=>console.log(error))
-      .finally(()=>setLoading(false))
+      .finally(()=>setStatus({loading:false,name}))
     }
 
-    function handleRemoveItem(productId:number ,quantity = 1){
-      setLoading(true);
+    function handleRemoveItem(productId:number ,quantity = 1,name:string){
+      setStatus({loading:true,name});
       agent.Basket.removeItem(productId,quantity)
       .then(()=>removeItem(productId,quantity))
       .catch(error=>console.log(error))
-      .finally(()=>setLoading(false))
+      .finally(()=>setStatus({loading:false,name}))
     }
 
     if(!basket) return <Typography variant = "h3">Your Basket is empty.</Typography>
 
     return (
-        <TableContainer component={Paper}>
+      <>
+      <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
@@ -52,24 +57,41 @@ export default function BasketPage(){
                 </TableCell>
                 <TableCell align="right">${(item.price/100).toFixed(2)}</TableCell>
                 <TableCell align="center">
-                  <LoadingButton loading = {loading} onClick = {()=>handleRemoveItem(item.productId)} color ="error">
+                  <LoadingButton 
+                  loading = {status.loading && status.name === "rem" + item.productId} 
+                  onClick = {()=>handleRemoveItem(item.productId,1,"rem"+ item.productId)} 
+                  color ="error">
                     <Remove />
                   </LoadingButton>
                   {item.quantity}
-                  <LoadingButton loading = {loading} onClick = {()=>handleAddItem(item.productId)} color ="secondary">
+                  <LoadingButton 
+                  loading = {status.loading && status.name === "add"+ item.productId} 
+                  onClick = {()=>handleAddItem(item.productId,"add"+item.productId)} 
+                  color ="secondary">
                     <Add />
                   </LoadingButton>
                 </TableCell>
                 <TableCell align="right">${((item.price/100)*item.quantity).toFixed(2)}</TableCell>
                 <TableCell align="right">
-                    <LoadingButton loading = {loading} onClick = {()=>handleRemoveItem(item.productId,item.quantity)} color = "error">
-                        <Delete />
-                    </LoadingButton>
+                <LoadingButton 
+                  loading = {status.loading && status.name === "del" + item.productId} 
+                  onClick = {()=>handleRemoveItem(item.productId,item.quantity,"del"+ item.productId)} 
+                  color ="error">
+                    <Delete />
+                  </LoadingButton>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <Grid container>
+        <Grid item xs={6}/>
+        <Grid item xs={6}>
+          <BasketSummary />
+        </Grid>
+      </Grid>
+      </>
+        
     );
 }
